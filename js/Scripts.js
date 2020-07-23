77655b14a47f75196714e7cec95f2bd9bd4241de
KennyArmasMantilla <60169312+KennyArmasMantilla@users.noreply.github.com>
2020-07-23 10:08:17 -0500

    1.3

---
+++ js/Scripts.js
@@ -0,0 +1,12 @@
+// alert("Hola mundo")
+
+/* Variables  */
+let numero=5
+console.log(numero)
+console.log(numero+1)
+console.log(numero*2)
+console.log(numero/3)
+
+if(10>numero){
+    console.log(numero)
+}
