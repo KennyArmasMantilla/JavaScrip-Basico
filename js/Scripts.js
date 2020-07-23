fa00f8759d149db6ace78bb56ab87980941b2d61
KennyArmasMantilla <60169312+KennyArmasMantilla@users.noreply.github.com>
2020-07-23 12:10:25 -0500

    Update Scripts.js

--- js/Scripts.js
+++ js/Scripts.js
@@ -5,6 +5,7 @@
 "nombre de la variable" sin espacios ni # adelante
 lueego le asignamos un valor =*/
 const numeros =20
+const edad=60
 
 let numero=10,
     nombre ='Luis',
@@ -21,3 +22,19 @@ console.log(numero/3)
 if(10>numero){
     console.log(numero)
 }
+
+/*-----------------tipos de datos----------------*/
+/*------ Primitivos ---------*/
+/*numeros: son numeros nada mas*/
+let numero =10
+/*string son textos, que se ponen en comillas "" `` ''*/
+let palabra ="letras"
+/*Los boolean  */
+boolean
+undefined
+null
+symbol
+
+
+
+/*------ Compuestos ---------*/
